7378de94337669cf8d97bbcd34517168ec42cc5c
James Percent <james@syndeticlogic.org>
2014-05-08 18:42:34 -0400

    Existing SQL stuff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 foneem
 ======
+
+Server for the foneem project
+
+
+
---
+++ foneem/sql/drop_ddl.sql
@@ -0,0 +1,12 @@
+begin;
+drop table if exists address;
+drop table if exists sentence_ordering;
+drop table if exists sentence_phoneme;
+drop table if exists user_sentence;
+
+drop table if exists users;
+drop table if exists sentences;
+drop table if exists phonemes;
+drop table if exists form_data;
+drop table if exists recipient;
+commit;                
