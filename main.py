8ba7f58acaa3eaf99cec88c43cd153e75bd3a6ce
James Percent <james@syndeticlogic.org>
2014-06-03 17:30:05 -0400

    Modified flask entry point to add the secret key from the config to the app

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-from foneem import app
+from foneem import app, parse_config
+
+app.hvb_conf = parse_config()
+app.secret_key = app.hvb_conf['app']['secret_key']
 app.run(host='0.0.0.0', port=int("80"), debug=True)
+
