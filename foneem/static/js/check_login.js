1155b46548491fe51e656f961e6f2fca12a9906b
James Percent <james@syndeticlogic.org>
2014-06-04 23:32:51 -0400

    Added jquery login stuffs

---
+++ foneem/static/js/check_login.js
@@ -0,0 +1,38 @@
+
+var loginChecker = {};
+(function(exports) {
+	exports.bindLogin = function(e) {
+		$.ajax({ url: '/is_teh_user_logged_in', 
+			async: false,
+			dataType: 'json',
+			success: function(data) {
+				console.log("data = ", data);
+				if (data.status == true) {
+					alert('true calse');
+					if (e.stopPropagation) {							
+						e.stopPropagation();
+					} else {
+						e.cancelBubble = true;
+					}
+					window.location = "record";							
+					return false;
+				} else {
+					alert("false");
+					return true;					
+				}
+			}
+		});			        			
+    };	
+	
+    exports.bindClickEvents = function() {     
+        $('#hvb-record-link').click(exports.bindLogin);
+		$('#hvb-record-button').click(exports.bindLogin);
+		$('#hvb-record-ways-to-help').click(exports.bindLogin);
+    };
+}(loginChecker));
+
+$( document ).ready(function() {
+    loginChecker.bindClickEvents();
+    console.log("initialization completed");
+});
+
