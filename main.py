d7387d2b015ae0ab39e69d49fe3163d935a72539
James Percent <james@syndeticlogic.net>
2014-06-12 17:03:03 -0400

    Changed port to 5000

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from foneem import app, parse_config
 
 app.hvb_conf = parse_config()
 app.secret_key = app.hvb_conf['app']['secret_key']
-app.run(host='0.0.0.0', port=int("80"), debug=True)
+app.run(host='0.0.0.0', port=int("5000"), debug=True)
 
