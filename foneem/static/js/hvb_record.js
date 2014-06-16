d5c9fde44810324a74fe1f1260ee4f84a229ecfe
James Percent <james@syndeticlogic.net>
2014-06-15 20:19:21 -0400

    Added the beginings of hvbOpacityManager - it will change opacity of the backwall based on the phonemes completed. Also fixed formatting.

--- foneem/static/js/hvb_record.js
+++ foneem/static/js/hvb_record.js
@@ -208,13 +208,22 @@ var recorder = {};
     }
 }(recorder));
 
+var hvbOpacityManager = {};
+(function(exports) {
+    exports.updateOpacity = function() {
+        var img = document.getElementById('rs5s1');
+        img.style.filter       = "alpha(opacity=25);";
+        img.style.MozOpacity   = 0.25;
+        img.style.opacity      = 0.25;
+        img.style.KhtmlOpacity = 0.25;
+    };
+}(hvbOpacityManager));
 
 var hvbButtonManager = {};
 (function(exports) {
     exports.bindClickEvents = function() {     
         $('#hvb_playsentence').click(function(e) {
             var sentence = hvbSentenceManager.getSentence();
-//            console.log("the sentence = ", sentence)
             var msg = new SpeechSynthesisUtterance(sentence.trim());
             window.speechSynthesis.speak(msg);
         });
