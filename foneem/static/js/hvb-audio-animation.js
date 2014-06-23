71d0931003038c2bafb0d53368b672b495e8148a
James Percent <james@syndeticlogic.org>
2014-06-23 18:06:12 -0400

    Encapuslated the base animation code

---
+++ foneem/static/js/hvb-audio-animation.js
@@ -0,0 +1,106 @@
+// Copyright (c) James Percent.
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//    1. Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//
+//    2. Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//    3. Neither the name of Unlock nor the names of its contributors may be used
+//       to endorse or promote products derived from this software without
+//       specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+hvb_audio_animation = {};
+(function(self) {
+    self.rafID = null;
+    self.analyserNode = null;
+    self.animate = false;
+
+    self.drawAnimation = function(canvasWidth, canvasHeight, analyserContext) {
+        if(self.animate) {
+            var distribution = 3;
+            var barWidth = 1;
+            var bars = Math.round(canvasWidth/distribution);
+            var freqByteData = new Uint8Array(self.analyserNode.frequencyBinCount);
+
+            self.analyserNode.getByteFrequencyData(freqByteData);
+
+            analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
+            analyserContext.fillStyle = '#F6D565';
+            analyserContext.lineCap = 'round';
+            var multiplier = self.analyserNode.frequencyBinCount / bars;
+
+            // Draw rectangle for each frequency bin.
+            for (var i = 0; i < bars; ++i) {
+                var magnitude = 0;
+                var offset = Math.floor( i * multiplier );
+
+                // sum/average the block, or we miss narrow-bandwidth spikes
+                for (var j = 0; j< multiplier; j++) {
+                    magnitude += freqByteData[offset + j];
+                }
+                magnitude = magnitude / multiplier;
+                var magnitude2 = freqByteData[i * multiplier];
+                analyserContext.fillStyle = "hsl( " + Math.round((i*360)/bars) + ", 100%, 50%)";
+                analyserContext.fillRect(i * distribution, canvasHeight, barWidth, -magnitude);
+            }
+        }
+        self.rafID = window.requestAnimationFrame(self.updateAnimation);
+    };
+
+    self.updateAnimation = function(time) {
+        try {
+            var canvas = document.getElementById("hvb-analyser");
+            var canvasWidth = canvas.width;
+            var canvasHeight = canvas.height;
+            var analyserContext = canvas.getContext('2d');
+            self.drawAnimation(canvasWidth, canvasHeight, analyserContext);
+        } catch (e) {
+            console.log("updateAnalysers Exception: ", e);
+        }
+    };
+
+    self.flipAnimationState = function() {
+        if(self.animate) {
+            self.animate = false;
+        } else {
+            self.animate = true;
+        }
+    };
+
+    self.cancelAnimationFrame = function() {
+        window.cancelRequestAnimationFrame(self.rafID);
+        self.rafID = null;
+    };
+
+
+    self.init = function(rafID, analyserNode) {
+        self.rafID = rafID;
+        self.analyserNode = analyserNode;
+        if (!navigator.cancelAnimationFrame) {
+            navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
+        }
+        if (!navigator.requestAnimationFrame) {
+            navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
+        }
+        self.updateAnimation();
+    };
+
+}(hvb_audio_animation));
+
+window.hvb_audio_animation = hvb_audio_animation;
