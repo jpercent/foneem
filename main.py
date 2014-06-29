3ff5b9a63be22ac89fe5972d49e87e18056d32ec
James Percent <james@syndeticlogic.net>
2014-06-28 20:23:35 -0400

    Integrated tornado and flask

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-#from tornado.wsgi import WSGIContainer
-#from tornado.httpserver import HTTPServer
-#from tornado.ioloop import IOLoop
-from foneem import app, parse_config
+# Copyright (c) 2014 James Percent. All rights reserved.
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#    3. The names of contributors may not be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-app.hvb_conf = parse_config()
-app.secret_key = app.hvb_conf['app']['secret_key']
-#http_server = HTTPServer(WSGIContainer(app))
+from foneem import app, parse_config, websock
+from tornado.wsgi import WSGIContainer
 
-#http_server.listen(5000)
-#IOLoop.instance().start()
-app.run(host='0.0.0.0', port=int("5000"), debug=True)
 
+def main():
+    app.hvb_conf = parse_config()
+    app.secret_key = app.hvb_conf['app']['secret_key']
+    wsgi_app = WSGIContainer(app)
+    server = websock.TornadoWebsocketServer(wsgi_app)
+    server.start()
+
+
+if __name__ == '__main__':
+    main()
