0712515923c8103219216a5531008fd2a3565c7f
James Percent <james@syndeticlogic.net>
2014-06-30 13:45:30 -0400

    Cosmetic changes

--- foneem/websock.py
+++ foneem/websock.py
@@ -71,18 +71,13 @@ class TornadoWSHandler(WebSocketHandler):
 
     def on_message(self, incoming):
         print 'message received %s' % incoming
-
-
-
         text = json.loads(incoming).get('text', None)
         msg = text if text else 'Sorry could you repeat?'
-
         response = json.dumps(dict(output='Parrot: {0}'.format(msg)))
         self.write_message(response)
 
     def on_close(self):
         print 'connection closed'
-
 
     def set_app(self, app):
         self.app = app
