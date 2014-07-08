b71fcbf7df6ac6f1d559597f659d1f52cc32b6bc
James Percent <james@syndeticlogic.org>
2014-07-07 21:31:17 -0400

    DDL changes

--- foneem/sql/create_ddl.sql
+++ foneem/sql/create_ddl.sql
@@ -1,45 +1,18 @@
 begin;
 
-----------------------------------------------------------
-
---
--- Table structure for table `recipient`
---
-
--- DROP TABLE IF EXISTS recipient;
-CREATE TABLE IF NOT EXISTS recipient (
-  recnum serial primary key,
-  "name" varchar(128) DEFAULT NULL,
-  email varchar(128) NOT NULL,
-  sex varchar(8) NOT NULL,
-  diagnosis varchar(64) NOT NULL,
-  ability varchar(64) NOT NULL
+create table if not exists users(
+    id serial primary key,
+    email text not null unique,
+    firstname varchar(80),
+    lastname varchar(80),
+    dob date,
+    gender varchar(10),
+    stateprovince varchar(128),
+    country varchar(128),
+    password text,
+    compendium text,
+    creation_time timestamp default current_timestamp
 );
-
---
--- Table structure for table `form_data`
---
-
--- DROP TABLE IF EXISTS form_data;
-CREATE TABLE IF NOT EXISTS form_data (
-  recnum serial primary key,
-  "name" varchar(128) DEFAULT NULL,
-  email varchar(128) NOT NULL,
-  phone varchar(16) DEFAULT NULL,
-  "comment" text,
-  role varchar(64) NOT NULL,
-  d int NOT NULL DEFAULT '0',
-  p int NOT NULL DEFAULT '0',
-  s int NOT NULL DEFAULT '0',
-  f int NOT NULL DEFAULT '0',
-  o int NOT NULL DEFAULT '0',
-  batchmail int NOT NULL DEFAULT '0',
-  primary_language varchar(128) DEFAULT NULL,
-  other_language varchar(128) DEFAULT NULL,
-  age varchar(128) DEFAULT NULL,
-  state_province varchar(128) DEFAULT NULL
-);
-
 
 create table if not exists sentences(
     id serial primary key,
@@ -57,48 +30,29 @@ create table if not exists phonemes(
    symbol char(2)
 );
 
+create table if not exists session_info(
+   id serial primary key,
+   base_loudness number,
+   creation_time timestamp default current_timestamp
+)
+
+create table if not exists grid (
+   id serial primary key,
+   css_id varchar(5)
+);
+
 create table if not exists sentence_phoneme(
     sentence_id int references sentences(id),
     phoneme_id int references phonemes(id)
 );
 
-create table if not exists sentence_ordering(
-   id serial primary key,
-   sentence_id int references sentences(id)
-);
-
-create table if not exists users(
-    id serial primary key,
-    email text not null unique,
-    firstname varchar(80),
-    lastname varchar(80),
-    dob date,
-    gender varchar(10),
-    stateprovince varchar(128),
-    country varchar(128),
-    password text,
-    compendium text,
-    creation_time timestamp default current_timestamp
-);
-
-create table if not exists address (
-   id serial primary key,
-   user_id int references users(id),
-   address varchar(80),
-   state varchar(80),
-   postal_code varchar(10),
-   country varchar(80)
-);
-
-create table if not exists user_sentence(
+create table if not exists user_sentence_session(
     user_id int references users(id),
     sentence_id int references sentences(id)
-);
-
-
-create table if not exists grid (
-   id serial primary key,
-   css_id varchar(5)
+    session_noise_id int references session(id),
+    average_noise number,
+    uri text,
+    creation_time timestamp default current_timestamp
 );
 
 create table if not exists phoneme_grid(
@@ -112,6 +66,34 @@ create table if not exists user_grid_opacity(
     opacity real,
     increments int,
     instances int
+);
+
+CREATE TABLE IF NOT EXISTS recipient (
+  recnum serial primary key,
+  "name" varchar(128) DEFAULT NULL,
+  email varchar(128) NOT NULL,
+  sex varchar(8) NOT NULL,
+  diagnosis varchar(64) NOT NULL,
+  ability varchar(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS form_data (
+  recnum serial primary key,
+  "name" varchar(128) DEFAULT NULL,
+  email varchar(128) NOT NULL,
+  phone varchar(16) DEFAULT NULL,
+  "comment" text,
+  role varchar(64) NOT NULL,
+  d int NOT NULL DEFAULT '0',
+  p int NOT NULL DEFAULT '0',
+  s int NOT NULL DEFAULT '0',
+  f int NOT NULL DEFAULT '0',
+  o int NOT NULL DEFAULT '0',
+  batchmail int NOT NULL DEFAULT '0',
+  primary_language varchar(128) DEFAULT NULL,
+  other_language varchar(128) DEFAULT NULL,
+  age varchar(128) DEFAULT NULL,
+  state_province varchar(128) DEFAULT NULL
 );
 
 commit;                
--- foneem/sql/drop_ddl.sql
+++ foneem/sql/drop_ddl.sql
@@ -1,18 +1,17 @@
 begin;
-drop table if exists address;
-drop table if exists sentence_ordering;
 drop table if exists sentence_phoneme;
-drop table if exists user_sentence;
+drop table if exists user_sentence_session;
 drop table if exists user_grid_opacity;
-
 drop table if exists phoneme_grid;
-drop table if exists grid;
-drop table if exists phonemes;
 
 drop table if exists users;
 drop table if exists sentences;
+drop table if exists phonemes;
+drop table if exists grid;
 
+drop table if exists session_info;
 
 drop table if exists form_data;
 drop table if exists recipient;
+
 commit;                
